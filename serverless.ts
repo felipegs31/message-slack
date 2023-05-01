1c2338db614f128d7f15c40623d6a13ad1db27b8
Felipe Gaya Scandiffio <felipegs31@gmail.com>
2023-05-01 09:58:22 -0300

    add envs

--- serverless.ts
+++ serverless.ts
@@ -5,7 +5,8 @@ import hello from '@functions/hello';
 const serverlessConfiguration: AWS = {
   service: 'message-slack',
   frameworkVersion: '3',
-  plugins: ['serverless-esbuild'],
+  useDotenv: true,
+  plugins: ['serverless-esbuild', 'serverless-dotenv-plugin'],
   provider: {
     name: 'aws',
     runtime: 'nodejs14.x',
